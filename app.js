30262bdd74bc579ee4656eb2f0fa590c2f31d036
singhalansh155 <singhalansh155@gmail.com>
2024-12-26 13:50:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const cursor= document.querySelector("#cursor")
+const blur= document.querySelector("#cursor-blur")
+
+function func(dets){
+    cursor.style.left = dets.x +"px";
+    cursor.style.top = dets.y +"px";
+    blur.style.left = dets.x-200 +"px";
+    blur.style.top = dets.y -200+"px";
+}
+document.addEventListener("mousemove",func);
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#nav",{
+    backgroundColor : "#000",
+    duration:0.5,
+    height:"110px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+gsap.to("#main",{
+    backgroundColor : "#000",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
+
+gsap.from("#aboutus img, #aboutus-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#aboutus",
+        scroller:"body",
+        start:"top 60%",
+        end:"top 55%",
+        scrub:3
+    }
+})
+// gsap.from(".cards", {
+//     scale: 0.8,
+//     // opacity:0,
+//     duration: 1,
+//     stagger: 0.1,
+//     scrollTrigger: {
+//       trigger: ".cards",
+//       scroller: "body",
+//       // markers:false,
+//       start: "top 70%",
+//       end: "top 65%",
+//       scrub: 1,
+//     },
+//   });
+
+
+gsap.from("#colon1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+  gsap.from("#colon2", {
+    y: 70,
+    x: 70,
+    scrollTrigger: {
+      trigger: "#colon1",
+      scroller: "body",
+      // markers:true,
+      start: "top 55%",
+      end: "top 45%",
+      scrub: 4,
+    },
+  });
+  gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+      trigger: "#page4 h1",
+      scroller: "body",
+      // markers:true,
+      start: "top 75%",
+      end: "top 70%",
+      scrub: 3,
+    },
+  });
+  
